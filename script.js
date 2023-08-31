88bb522d23579ee1ac4b101b40a27d93cccb5cf7
brayang222 <116692605+brayang222@users.noreply.github.com>
2023-08-31 16:32:33 -0500

    index

--- script.js
+++ script.js
@@ -35,7 +35,15 @@ function processXml(xmlContent) {
   const cdataPattern = /<!\[CDATA\[(.*?)]]>/gs;
   const modifiedXml = xmlContent.replace(cdataPattern, function(match, cdataContent) {
     const modifiedCData = cdataContent.replace(/<\?xml[^?]*\?>/g, '');
-    return modifiedCData;
+
+    // Buscar y modificar las etiquetas según lo requerido
+    const modifiedWithConcatenation = modifiedCData.replace(/<cac:InvoiceLine>.*?<cac:Item>(?=.*?<cbc:Description>.*?<\/cbc:Description>.*?<cbc:Description>.*?<\/cbc:Description>).*?<\/cac:Item>.*?<\/cac:InvoiceLine>/gs,
+      function(match) {
+        const concatenatedDescription = match.replace(/<cbc:Description>(.*?)<\/cbc:Description>.*?<cbc:Description>(.*?)<\/cbc:Description>/gs, '<cbc:Description>$1 | $2<\/cbc:Description>'); // Anidar texto de ambas etiquetas
+        return concatenatedDescription;
+      });
+
+    return modifiedWithConcatenation;
   });
   return modifiedXml;
 }
